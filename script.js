4658eeb920665b40b05c13c9b7707d7fd6e76081
ibrahimavucan2004-png <ibrahimavucan2004@gmail.com>
2026-07-22 18:43:53 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function () {
+
+const bouton = document.querySelector("button");
+
+bouton.addEventListener("click", function () {
+alert("Bienvenue chez AVC Digital ! Nous allons créer votre projet numérique.");
+});
+
+});
